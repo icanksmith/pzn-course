eec1bda299ebca38efda40a844d924506ed84af9
icank smith <sammykitkat8@gmail.com>
2025-11-23 12:49:58 +0700

    belajar do while loop dan for of loop

---
+++ javascript-dasar/for-of-loop.js
@@ -0,0 +1,11 @@
+const data = ['keshi', 'Yung kai', 17, 19];
+
+for (const index of data){
+    console.log(index);
+}
+
+const name = 'icank smith';
+
+for (const iteration of name){
+    console.log(iteration);
+}
